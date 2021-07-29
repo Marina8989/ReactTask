58a5093aa302676b99259b488d8e9c164213c8a6
Marina8989 <marina.mill209@gmail.com>
2021-07-29 13:20:14 -0700

    react task tracker

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Button() {
+    return (
+        <>
+          <button className='addBtn'>Add</button>  
+        </>
+    )
+}
+
+export default Button
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from './Button';
+
+function Header() {
+    return (
+        <div className='header'>
+            <h2>Task Tracker</h2>
+            <Button />
+        </div>
+    )
+}
+
+export default Header
