bc235a42599a921e236ad10ade59c286dc2298ee
Marina8989 <marina.mill209@gmail.com>
2021-07-29 14:35:42 -0700

    react

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,39 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
+import {FaTimes} from 'react-icons/fa';
 
-
-function Task() {
-    const [data, setData] =useState([
-    {
-        id: 1,
-        text: 'Do Laundry',
-        complete: true
-    },
-    {
-        id: 2,
-        text: 'Grocery Shopping',
-        complete: false
-    },
-    {
-        id: 3,
-        text: 'Walk Dog',
-        complete: true
-    },
-    {
-        id: 4,
-        text: 'Clean House',
-        complete: true
-    },
-]);
-   
+function Task({text}) {
     return (
-        <>
-           {data.map(item => {
-              const {id, text} = item;
-              return (
-                  <h4 key={id}>{text}</h4>
-              )
-           })}
-        </>
+        <div className="task">
+          <h3>{text}</h3><FaTimes style={{color: 'red'}} className="icon"/>
+        </div>
     )
 }
 
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Task from './Task';
 
 
 function Tasks() {
+    const [data, setData] = useState([
+    {
+        id: 1,
+        text: 'Do Laundry',
+        complete: true
+    },
+    {
+        id: 2,
+        text: 'Grocery Shopping',
+        complete: false
+    },
+    {
+        id: 3,
+        text: 'Walk Dog',
+        complete: true
+    },
+    {
+        id: 4,
+        text: 'Clean House',
+        complete: true
+    },
+]);
+
     return (
-        <>
-          <Task />
-        </>
+        <div className="taskBlock">
+          {data.map(task => {
+              const {id, text} = task;
+              return (
+                  <Task key={id} text={text}/>
+              )
+          })}
+        </div>
     )
 }
 
